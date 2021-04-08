c095b8b069892a50e65330d104173694277607e1
yaelBrown <yaelrbrown@gmail.com>
2021-04-08 09:37:42 -0500

    add spring proj

--- set_aliases.sh
+++ set_aliases.sh
@@ -1,5 +1,5 @@
-echo "alias m=minikube" > ~/.bash_profile
-echo "alias k=kubernetes" > ~/.bash_profile
-echo "alias d=docker" > ~/.bash_profile
-echo "alias dc=docker-compose" > ~/.bash_profile
+echo "alias m=minikube" >> ~/.bash_profile
+echo "alias k=kubectl" >> ~/.bash_profile
+echo "alias d=docker" >> ~/.bash_profile
+echo "alias dc=docker-compose" >> ~/.bash_profile
 source ~/.bash_profile
